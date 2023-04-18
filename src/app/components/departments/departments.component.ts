cc885f0d377b6e3e900f4fbe50d153571884e28c
Joseph Burton <joseph.burton6967@stu.mtec.edu>
2023-04-17 23:50:15 -0600

    Unit 12 Using Async Pipe

--- src/app/components/departments/departments.component.ts
+++ src/app/components/departments/departments.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Department } from '../../interfaces/department';
 import { DepartmentsService } from '../../services/departments.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-departments',
@@ -9,7 +10,8 @@ import { DepartmentsService } from '../../services/departments.service';
   styleUrls: ['./departments.component.scss']
 })
 export class DepartmentsComponent {
-  departments: Department[]
+  // departments: Department[]
+  $departments: Observable<Department[]> | undefined;
 
   constructor(
     private departmentsService: DepartmentsService,
@@ -17,9 +19,11 @@ export class DepartmentsComponent {
   ) { }
 
   ngOnInit(): void {
-    this.departmentsService.getDepartments().subscribe(departments => {
-      this.departments = departments;
-    });
+    // this.departmentsService.getDepartments().subscribe(departments => {
+    //   this.departments = departments;
+    // });
+    this.$departments = this.departmentsService.getDepartments();
+    
   }
 
   goToDepartment(departmentId: string): void {
